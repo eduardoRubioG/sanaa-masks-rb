8ec7b6a19cc073747482d738f1e73db0376a0438
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2020-11-03 18:22:38 -0500

    Deployed live. Will work on setting up the build component now

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,48 @@
-import React from "react"
+import React from "react";
+import { graphql } from 'gatsby';
 
 // Components
-import Layout from "../components/layout"
+import Layout from "../components/layout";
 import Hero from "../components/Hero/hero";
 import About from "../components/About/About";
 import MaskSection from "../components/MaskSection/MaskSection";
 
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout pageInfo={{ pageName: "index" }}>
     <SEO title="Home" keywords={[`gatsby`, `react`, `bootstrap`]} />
     <Hero />
     <About />
-    <MaskSection />
+    <MaskSection priceData={data.allStripePrice} productData={data.allStripeProduct}/>
   </Layout>
-)
+);
+
+export const query = graphql`
+  query productQuery {
+    allStripePrice {
+      edges {
+        node {
+          id
+          product {
+            name
+            metadata {
+              type
+            }
+          }
+          unit_amount
+        }
+      }
+    }
+    allStripeProduct {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
